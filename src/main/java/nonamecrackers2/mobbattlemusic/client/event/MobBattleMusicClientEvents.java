187a75c9206e6a7732c4deee50050815f9c20459
nonamecrackers2 <nonamecrackers2@outlook.com>
2024-01-26 15:29:33 -0600

    Organization

--- src/main/java/nonamecrackers2/mobbattlemusic/client/event/MobBattleMusicClientEvents.java
+++ src/main/java/nonamecrackers2/mobbattlemusic/client/event/MobBattleMusicClientEvents.java
@@ -1,15 +1,7 @@
 package nonamecrackers2.mobbattlemusic.client.event;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.monster.Enemy;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.TieredItem;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import nonamecrackers2.mobbattlemusic.client.init.MobBattleMusicClientCapabilities;
 import nonamecrackers2.mobbattlemusic.client.manager.BattleMusicManager;
